4cb7042c3b2db57424e5402aac1a176fd7beac08
Carolinne Moreira <carolinnemoreira@yahoo.com.br>
2022-12-19 21:08:28 -0300

    CSS Modules

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Header } from './components/Header';
+import { Post } from './Post';
+
+import './styles.css';
+
+export function App() {
+
+  return (
+    /*<h1>Hello World!</h1>*/
+    <div>
+      <Header />
+
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  )
+}
+
